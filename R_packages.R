9c7841d43f647d2d74cccbbf5692b9cd8b67f7b4
vlengrand <vlengrand@ca-cf.fr>
2016-12-16 14:37:07 +0100

    Nouveau fichier

---
+++ R_packages.R
@@ -0,0 +1,10 @@
+install.packages("slidify")
+install.packages(c("slidify","ggplot2","devtools"))
+
+source("http://bioconductor.org/biocLite.R")
+biocLite()
+biocLite(c("GenomicFeatures","AnnotationDbi"))
+
+library(ggplot2)
+search()
+
